907b96253bab6aa10c171dffa38e1dc32db208a9
siarheishut <szut.siergiej@gmail.com>
2023-06-09 19:13:22 +0200

    Liken tests format.

--- tests/request_processor_test.cpp
+++ tests/request_processor_test.cpp
@@ -7,13 +7,15 @@
 #include <optional>
 
 TEST(TestProcessRequests, TestStopResponseToJson) {
+  using json::List;
+  using json::Dict;
   using ResponseOpt = std::optional<rm::StopResponse>;
 
   struct TestCase {
     std::string name;
     ResponseOpt response;
     int id;
-    json::Dict want;
+    Dict want;
   };
 
   std::vector<TestCase> test_cases{
@@ -21,39 +23,41 @@ TEST(TestProcessRequests, TestStopResponseToJson) {
           .name = "Unknown stop",
           .response = std::nullopt,
           .id = 94387,
-          .want = json::Dict{{"request_id", 94387},
-                             {"error_message", "not found"}},
+          .want = Dict{{"request_id", 94387},
+                       {"error_message", "not found"}},
       },
       TestCase{
           .name = "No buses",
           .response = {{}},
           .id = 5612022,
-          .want = json::Dict{{"request_id", 5612022},
-                             {"buses", json::List{}}},
+          .want = Dict{{"request_id", 5612022},
+                       {"buses", List{}}},
       },
       TestCase{
           .name = "Buses found",
           .response = rm::StopResponse{{"Bus1", "12464", "Bus 2"}},
           .id = 12,
-          .want = json::Dict{{"request_id", 12},
-                             {"buses", json::List{"Bus1", "12464", "Bus 2"}}},
+          .want = Dict{{"request_id", 12},
+                       {"buses", List{"Bus1", "12464", "Bus 2"}}},
       },
   };
 
   for (auto &[name, response, id, want] : test_cases) {
-    json::Dict got = rm::ToJson(response, id);
+    Dict got = rm::ToJson(response, id);
     EXPECT_EQ(want, got);
   }
 }
 
 TEST(TestProcessRequests, TestBusResponseToJson) {
+  using json::List;
+  using json::Dict;
   using ResponseOpt = std::optional<rm::BusResponse>;
 
   struct TestCase {
     std::string name;
     ResponseOpt response;
     int id;
-    json::Dict want;
+    Dict want;
   };
 
   std::vector<TestCase> test_cases{
@@ -61,8 +65,8 @@ TEST(TestProcessRequests, TestBusResponseToJson) {
           .name = "Unknown bus",
           .response = std::nullopt,
           .id = 1234567,
-          .want = json::Dict{{"request_id", 1234567},
-                             {"error_message", "not found"}},
+          .want = Dict{{"request_id", 1234567},
+                       {"error_message", "not found"}},
       },
       TestCase{
           .name = "Bus Found",
@@ -73,16 +77,16 @@ TEST(TestProcessRequests, TestBusResponseToJson) {
               .curvature = 1.54712,
           },
           .id = 1407207,
-          .want = json::Dict{{"route_length", 194271.1},
-                             {"request_id", 1407207},
-                             {"curvature", 1.54712},
-                             {"stop_count", 5},
-                             {"unique_stop_count", 4}},
+          .want = Dict{{"route_length", 194271.1},
+                       {"request_id", 1407207},
+                       {"curvature", 1.54712},
+                       {"stop_count", 5},
+                       {"unique_stop_count", 4}},
       },
   };
 
   for (auto &[name, response, id, want] : test_cases) {
-    json::Dict got = rm::ToJson(response, id);
+    Dict got = rm::ToJson(response, id);
     EXPECT_EQ(want, got);
   }
 }
