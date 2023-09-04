5d40c4df089743cd7f2edf9d56fdf49a9c2097a5
siarheishut <szut.siergiej@gmail.com>
2023-09-04 14:11:30 +0200

    Separate common utilities.

---
+++ src/common.h
@@ -0,0 +1,31 @@
+#ifndef ROOT_MANAGER_SRC_COMMON_H_
+#define ROOT_MANAGER_SRC_COMMON_H_
+
+#include "request_types.h"
+
+#include <vector>
+#include <string>
+#include <variant>
+#include <unordered_map>
+
+namespace rm {
+struct StopInfo {
+  using Dists = std::unordered_map<std::string, int>;
+  // Distances to other stops.
+  Dists dists;
+  Coords coords;
+  std::vector<std::string> buses;
+};
+
+struct BusInfo {
+  std::vector<std::string> stops;
+  int unique_stop_count;
+  double distance;
+  double curvature;
+};
+
+using StopDict = std::unordered_map<std::string, StopInfo>;
+using BusDict = std::unordered_map<std::string, BusInfo>;
+}
+
+#endif // ROOT_MANAGER_SRC_COMMON_H_
