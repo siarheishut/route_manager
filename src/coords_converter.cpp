2ff9d748b6e5f2058fe8501d963437cb5966790f
siarheishut <szut.siergiej@gmail.com>
2023-10-24 22:57:35 +0200

    Add functions to find base stops.

--- src/coords_converter.cpp
+++ src/coords_converter.cpp
@@ -10,24 +10,64 @@
 
 #include "request_types.h"
 
-using namespace std;
-
 namespace rm::coords_converter {
-vector <string_view>
+std::vector<std::string_view>
 SortStops(const renderer_utils::Stops &stops, SortMode mode) {
-  vector<pair<double, string_view>> sorted_stops;
+  std::vector<std::pair<double, std::string_view>> sorted_stops;
   transform(begin(stops), end(stops), back_inserter(sorted_stops),
             [mode](auto &item) {
               if (mode == SortMode::kByLatitude)
-                return pair{item.second.latitude, item.first};
-              return pair{item.second.longitude, item.first};
+                return std::pair{item.second.latitude, item.first};
+              return std::pair{item.second.longitude, item.first};
             });
   sort(begin(sorted_stops), end(sorted_stops));
 
-  vector<string_view> layers;
+  std::vector<std::string_view> layers;
   transform(begin(sorted_stops), end(sorted_stops), back_inserter(layers),
             [](auto &item) { return item.second; });
   return layers;
+}
+
+std::unordered_set<std::string_view>
+IntersectionsWithinRoute(const renderer_utils::Buses &buses, int count) {
+  std::unordered_set < std::string_view > base_stops;
+  for (auto &[_, route] : buses) {
+    std::unordered_map<std::string_view, int> stop_freqs;
+    for (int i = 0; i < route.route.size(); ++i)
+      stop_freqs[route.route[i]] +=
+          (!route.is_roundtrip && i < route.route.size() - 1 ? 2 : 1);
+
+    for (auto &[stop, freq] : stop_freqs)
+      if (freq >= count)
+        base_stops.insert(stop);
+  }
+  return base_stops;
+}
+
+std::unordered_set<std::string_view>
+EndPoints(const renderer_utils::Buses &buses) {
+  std::unordered_set < std::string_view > base_stops;
+  for (auto &[_, route] : buses) {
+    base_stops.insert(route.route.front());
+    if (!route.is_roundtrip)
+      base_stops.insert(route.route.back());
+  }
+  return base_stops;
+}
+
+std::unordered_set<std::string_view>
+IntersectionsCrossRoute(const renderer_utils::Buses &buses) {
+  std::unordered_set < std::string_view > base_stops, visited;
+  for (auto &[_, route] : buses) {
+    for (auto stop : route.route)
+      if (visited.find(stop) != visited.end())
+        base_stops.insert(stop);
+
+    for (auto stop : route.route) {
+      visited.insert(stop);
+    }
+  }
+  return base_stops;
 }
 
 renderer_utils::Stops Interpolate(
@@ -63,9 +103,9 @@ renderer_utils::Stops Interpolate(
   return std::move(stops);
 }
 
-vector <pair<string_view, string_view>>
+std::vector<std::pair<std::string_view, std::string_view>>
 AdjacentStops(const renderer_utils::Buses &buses) {
-  vector<pair<string_view, string_view>> result;
+  std::vector<std::pair<std::string_view, std::string_view>> result;
   for (auto &[_, route] : buses) {
     for (int i = 1; i < route.route.size(); ++i) {
       auto prev = route.route[i - 1];
@@ -77,24 +117,24 @@ AdjacentStops(const renderer_utils::Buses &buses) {
   return result;
 }
 
-vector <vector<string_view>> CompressNonadjacent(
-    const vector <string_view> &stops,
-    const vector <pair<string_view,
-                       string_view>> &neighbors) {
-  unordered_map<string_view, int> stop_to_layer;
+std::vector<std::vector<std::string_view>> CompressNonadjacent(
+    const std::vector<std::string_view> &stops,
+    const std::vector<std::pair<std::string_view,
+                                std::string_view>> &neighbors) {
+  std::unordered_map<std::string_view, int> stop_to_layer;
   for (int i = 0; i < stops.size(); ++i)
     stop_to_layer[stops[i]] = i;
 
-  vector<int> max_next(stops.size(), static_cast<int>(stops.size()) - 1);
+  std::vector<int> max_next(stops.size(), static_cast<int>(stops.size()) - 1);
 
   for (auto &[prev, curr] : neighbors) {
     auto prev_layer = stop_to_layer[prev];
     auto curr_layer = stop_to_layer[curr];
-    if (prev_layer > curr_layer) swap(prev_layer, curr_layer);
-    max_next[prev_layer] = min(max_next[prev_layer], curr_layer - 1);
+    if (prev_layer > curr_layer) std::swap(prev_layer, curr_layer);
+    max_next[prev_layer] = std::min(max_next[prev_layer], curr_layer - 1);
   }
 
-  vector<vector<string_view>> res;
+  std::vector<std::vector<std::string_view>> res;
   int last = -1;
   for (int layer = 0; layer < stops.size(); ++layer) {
     if (layer > last) {
@@ -102,18 +142,18 @@ vector <vector<string_view>> CompressNonadjacent(
       last = max_next[layer];
       continue;
     }
-    last = min(last, max_next[layer]);
+    last = std::min(last, max_next[layer]);
     res.back().push_back(stops[layer]);
   }
   return res;
 }
 
-vector <pair<string_view, double>> SpreadStops(
-    const vector <vector<string_view>> &layers,
+std::vector<std::pair<std::string_view, double>> SpreadStops(
+    const std::vector<std::vector<std::string_view>> &layers,
     double from, double to) {
   int stop_count = layers.size();
   double step = (stop_count == 1) ? 0.0 : (to - from) / (stop_count - 1);
-  vector<pair<string_view, double>> result;
+  std::vector<std::pair<std::string_view, double>> result;
   for (int i = 0; i < layers.size(); ++i)
     for (auto stop : layers[i])
       result.emplace_back(stop, from + step * i);
