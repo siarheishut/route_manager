ec65a2653381a6ba0c5f29737e0966d07c4a551f
siarheishut <szut.siergiej@gmail.com>
2024-01-26 09:27:50 +0100

    Add Serialization for RouteManager.
    Add serialization and deserialization for RouteManager.

--- src/route_manager.h
+++ src/route_manager.h
@@ -4,6 +4,7 @@
 #include <memory>
 #include <optional>
 #include <string>
+#include <tuple>
 #include <unordered_map>
 #include <variant>
 #include <vector>
@@ -11,12 +12,19 @@
 #include "router.h"
 
 #include "common.h"
+#include "graph.h"
 #include "request_types.h"
 #include "route_manager_utils.h"
+#include "transport_catalog.h"
+#include "transport_catalog.pb.h"
 
 namespace rm {
 class RouteManager {
  public:
+  static std::unique_ptr<RouteManager> Deserialize(
+      const ::TransportCatalog::TransportCatalog &proto_catalog,
+      std::shared_ptr<TransportCatalog> catalog);
+
   RouteManager(const rm::utils::StopDict &stop_info,
                const rm::utils::BusDict &bus_info,
                const rm::utils::RoutingSettings &routing_settings);
@@ -24,11 +32,27 @@ class RouteManager {
   std::optional<utils::RouteInfo> FindRoute(const std::string &from,
                                             const std::string &to);
 
+  ::TransportCatalog::Router Serialize(
+      const ::TransportCatalog::TransportDatabase &proto_db) const;
+
  private:
+  struct Params {
+    std::shared_ptr<rm::TransportCatalog> catalog;
+    std::unique_ptr<graph::Router<double>> router;
+    graph::DirectedWeightedGraph<double> graph;
+    rm::utils::RoutingSettings settings;
+    std::vector<rm::route_manager::Edge> edges;
+    std::vector<std::string> stop_names;
+    std::unordered_map<std::string, size_t> stop_ids;
+  };
+
+  explicit RouteManager(Params config);
+
   void ReadStops(const rm::utils::StopDict &stop_dict);
   void ReadBuses(const rm::utils::BusDict &stop_dict,
                  const rm::utils::StopDict &bus_dict);
 
+  std::shared_ptr<TransportCatalog> catalog_;
   std::unique_ptr<route_manager::Router> router_;
   route_manager::Graph graph_;
   rm::utils::RoutingSettings settings_;
